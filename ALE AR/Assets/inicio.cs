e30d2a02d189367cbdc34bb1f36c648543800867
SabianB <villacreses_xavier@yahoo.es>
2022-11-23 15:03:30 -0500

    MainMenu

---
+++ ALE AR/Assets/inicio.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inicio : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
